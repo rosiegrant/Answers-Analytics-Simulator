538177bbe5df0acc30373f17319035935265bde9
rosiegrant <rosellen.grant@gmail.com>
2020-12-20 14:17:33 -0500

    cleaning up

--- selenium.js
+++ selenium.js
@@ -10,19 +10,14 @@ const driver = new webdriver.Builder()
 driver.manage().setTimeouts({implicit: 10000});
 driver.get('https://synergic-health.yextdemos.com/');
 
-console.log("hello!");
-
+//start interaction
 driver.findElement(By.className('js-yext-query')).sendKeys('pay my bill');
 driver.findElement(By.className('js-yext-query')).sendKeys(webdriver.Key.ENTER);
 
-
-// let element = driver.wait(until.elementLocated(By.className('js-HitchhikerFaqAccordion-toggle')), 10000, "timed out");
-// element.click;
-driver.sleep(10000).then(function()  {
+driver.sleep(15000).then(function()  {
     driver.findElement(By.className('js-HitchhikerFaqAccordion-toggle')).click();
-    
 })
 
-driver.sleep(11000).then(function() {
+driver.sleep(20000).then(function() {
     driver.quit();
 });
